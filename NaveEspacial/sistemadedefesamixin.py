f408524e5e2e85774aa9127da4cd95cdfcf04f23
mateus <mateus.ssi.gomes@gmail.com>
2026-05-20 21:01:49 -0300

    Atualização

---
+++ NaveEspacial/sistemadedefesamixin.py
@@ -0,0 +1,6 @@
+class SistemaDeDefesaMixin:
+    def ativar_escudos(self):
+        print('Ativando escudos da nave')
+        
+    def desligare_scudos(self):
+        print('Desligando escudos da nave')
